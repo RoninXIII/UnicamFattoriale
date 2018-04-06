c92590550d7bee6f52dafe27e4028c67628fc900
Mario Lombardi <mario.lombardi@studenti.unicam.it>
2018-04-06 18:28:27 +0200

    The program respects all the requests

--- Program.cs
+++ Program.cs
@@ -1,49 +1,60 @@
 ﻿using System;
+using System.Numerics;
 
-namespace UnicamFattoriale
+namespace unicamFattoriale
 {
-    class Program
+
+    class unicamFattoriale
     {
         static void Main(string[] args)
         {
-            //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
             //Print some helpful messages to the user, so he will know what he should do
             
-            //This program calculates the factorial of a number
-            Console.WriteLine("Questo programma calcola il fattoriale di un numero");
-            //Type a number then press Enter
-            Console.Write("Digita un numero e premi invio: ");
+            
+            Console.WriteLine("This program calculates the factorial of a number");
+            
+            Console.WriteLine("Press any keys to continue or Esc to exit: ");
+            //If you will press the Esc button the program will stop immediately.
+            //The while doesn't let the program to stop after every execution
+            while(Console.ReadKey().Key != ConsoleKey.Escape){
+                
+            Console.Write("Type a number then press Enter.");
 
-            //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
-            //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
             
             //By using Console.ReadLine, we wait for the user until he types something and presses Enter.
             //When he/she's pressed enter, the text he/she typed will be assigned to a string variable
             string testoDigitatoDallUtente = Console.ReadLine();
 
-            //Interpreto il testo come un numero intero
-            //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
-            //che non è interpretabile come un numero! (Es. ABCD)
-            
-            //Let's convert the text to an integer
-            //Beware: this instruction will fail if the user has input a text
-            //which is not convertible to a number! (e.g. ABCD)
-            int numero = int.Parse(testoDigitatoDallUtente);
-
-            //Calcolo il fattoriale: qui ci sono degli errori!
-            //Factorial calculation: this is currently wrong!
-            int risultato = 0;
-            while (numero >= 0) {
+            //Let's convert the text to an integer; using the uint type the user is not able to type negative numbers.
+            //The try-catch block is needed to handle the exception for an invalid input.
+            try{
+            uint numero = uint.Parse(testoDigitatoDallUtente);
+          
+            BigInteger risultato=1;
+            while (numero > 1) {
                 risultato *= numero;
                 numero--;
             }
-
-            //Stampo il risultato
+            //The factorial for 0 number is 1
+            if(numero==0){
+                risultato=1;
+            }
+           
             //Print the result
-            Console.WriteLine($"Il fattoriale di {numero} e' {risultato}");
-            //Attendo che l'utente prema un tasto prima di uscire
-            //Let's wait for another Enter, or the program would close and we wouldn't see its output
-            Console.ReadKey();
+            Console.WriteLine($"The factorial of {testoDigitatoDallUtente} is {risultato}");
+
+            //This message will show itself after every execution.
+            Console.WriteLine("Press any keys to continue or Esc to exit: ");
+           
+            //If the input is invalid the program will exit.
+            }catch{
+                Console.WriteLine("Sorry, the input is invalid!!");
+                  Console.ReadKey();
+                return;
+            }
+            }
+
+           
         }
-    }
+}
 }
